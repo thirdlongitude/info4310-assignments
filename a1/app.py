27bf2aaf939de6938acc3026a6b38c0b6ed31b57
Yvette Hung <yh387@cornell.edu>
2023-02-05 18:28:57 -0500

    initial files

---
+++ a1/app.py
@@ -0,0 +1,13 @@
+from flask import *
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root = 'static/', prefix = 'static/', index_file = 'index.htm', autorefresh = True)
+
+@app.route('/', methods = ['GET'])
+
+def hello():
+  return make_response('hello world')
+
+if __name__ == '__main__':
+  app.run(threaded = True, port = 5000)
